afed3b80fa02ba1aa9612c830446dd7260207d30
TimHarrell <mrmath1618@gmail.com>
2018-08-21 13:50:55 -0500

    working on retrieving skills

--- dndhelper/src/main/java/com/revature/dndhelper/dao/CharacterDao.java
+++ dndhelper/src/main/java/com/revature/dndhelper/dao/CharacterDao.java
@@ -8,6 +8,8 @@ import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.springframework.stereotype.Repository;
 
+import com.revature.dndhelper.beans.CharacterSkills;
+
 @Repository
 public class CharacterDao {
 	private SessionFactory sessionFactory;
@@ -21,12 +23,25 @@ public class CharacterDao {
 		return s.createQuery("from Character").list();
 	}
 	
-	public List<Character> getCharactersByuserId(String userId) {
+	public List<Character> getCharactersByUserId(String userId) {
 		Session s = sessionFactory.getCurrentSession();
 		List<Character> result = s.createQuery("from Character where user_id= :userId").
 				setString("userId", userId).list();
 	
 		return result;
+	}
+	
+	public HashMap<String, Boolean> getCharacterSkillsByCharacterId(int charId) {
+		Session s = sessionFactory.getCurrentSession();
+		List<CharacterSkills> result = s.createQuery("from CharacterSkills where char_id= :charId").
+				setInteger("charId", charId).list();
+		
+		if(result.isEmpty()) {
+			return null; // charId is not in table 
+		}
+		CharacterSkills skills = result.get(0); // there will only be one skill row retrieved
+		// finish this by returning characterSkills as some sort of ordered iterable Collection
+		
 	}
 	
 	public int saveCharacter(Character c) {
@@ -36,4 +51,6 @@ public class CharacterDao {
 		tx.commit();
 		return result;
 	}
+	
+	
 }
