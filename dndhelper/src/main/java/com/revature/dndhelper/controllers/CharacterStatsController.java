40c5a95e780d8a6439e5658c2a5b28794ee5dceb
andersen99 <32144878+andersen99@users.noreply.github.com>
2018-08-22 23:37:20 -0500

    added two new controllers

---
+++ dndhelper/src/main/java/com/revature/dndhelper/controllers/CharacterStatsController.java
@@ -0,0 +1,21 @@
+package com.revature.dndhelper.controllers;
+
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.HttpServletBean;
+import com.revature.dndhelper.beans.CharacterStats;
+
+@RestController("/characterStats")
+public class CharacterStatsController{
+	
+	@RequestMapping(value = "/assignStats", method = RequestMethod.POST)
+	public void assignStats(@PathVariable("charStats") CharacterStats charStats) {
+		// if we need to use the controller to store the character stats it will be done here
+		// otherwise we should not need this controller
+	}
+}
